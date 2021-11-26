ee9093bef2245124e54d8221172c223803b94bd6
BLOX <92363849+SCV2PL@users.noreply.github.com>
2021-11-26 15:30:42 +0100

    Add files via upload

---
+++ clear_districts_vaccination_range.py
@@ -0,0 +1,24 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = '/home/luke_blue/Startup_Files/sars-cov-2-poland.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1JshfkqgC8bLhATHHkN3D5Bto19Sp3BpMBTYuAts5z_c'
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+                               
+request1 = sheet.values().clear(spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+                               range="XY!H2:H380").execute()
+                              
+print(request1)
+
+# python3 /home/luke_blue/Startup_Files/clear_districts_vaccination_range.py
---
+++ clear_vaccination.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python3 /home/luke_blue/Startup_Files/clear_avr7d10000_vaccination_range.py
+
+python3 /home/luke_blue/Startup_Files/clear_districts_vaccination_range.py
+
+# /home/luke_blue/Startup_Files/clear_vaccination.sh
