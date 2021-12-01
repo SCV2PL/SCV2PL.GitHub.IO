fbc11bc19ed24434fe6c414974b3db896eeed745
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-12-01 19:03:21 +0100

    Add files via upload

--- update_districts_avrxypl.py
+++ update_districts_avrxypl.py
@@ -19,7 +19,7 @@ result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                             range="avrxypl!J1").execute()
 values = result.get('values', [])
 
-RUN = [["=({'2021-11-21'!D2}+{'2021-11-22'!D2}+{'2021-11-23'!D2}+{'2021-11-24'!D2}+{'2021-11-25'!D2}+{'2021-11-26'!D2}+{'2021-11-27'!D2})/7"]]
+RUN = [["=({'2021-11-26'!D2}+{'2021-11-27'!D2}+{'2021-11-28'!D2}+{'2021-11-29'!D2}+{'2021-11-30'!D2}+{'2021-12-01'!D2}+{'2021-12-02'!D2})/7"]]
 
 request = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,
                                                  range="avrxypl!J1", valueInputOption="USER_ENTERED", body={"values":RUN}).execute()
