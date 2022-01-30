7205e078396a1f4ea64d16b0f5b2d8f9414bc1a0
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2022-01-30 18:47:29 +0100

    Delete sunday_update_for_cron_schedule.sh

--- sunday_update_for_cron_schedule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 /home/luke_blue/Startup_Files/sunday_update/add_weekly_sheets_set.py
-
-python3 /home/luke_blue/Startup_Files/sunday_update/update_avr7d_array_range_7d.py
-
-# chmod u+r+x 
-# /home/luke_blue/Startup_Files/sunday_update/sunday_update_for_cron_schedule.sh
