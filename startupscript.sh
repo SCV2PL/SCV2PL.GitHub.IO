115d818414f8b03b1fd21e75b7b4ff1f2e79aec0
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-02-11 10:58:40 +0100

    Add files via upload

--- startupscript.sh
+++ startupscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 2022-01-29 - full automation for daily updates, lines: 18, 21, 29, 37 , 36, 47, 98, 106, 111, 117, 168, 171
 #python3 /home/luke_blue/Startup_Files/import_csv_data_from_urls.py
-# 2022-02-08 22:32:00 Evening UPGRADE_2.0_APP Complete
+# 2022-02-11 10:27:00 UPGRADE_2.0_APP Complete
 python3 /home/luke_blue/Startup_Files/UPGRADE_2.0_APP/import_csv_data_from_urls.py
 # 2022-01-30 - full automation for daily updates, lines: 22, 32
 #python3 /home/luke_blue/Startup_Files/UPGRADE_2.0_APP/update_districts_j1_xy_d3_todaycount.py
@@ -32,7 +32,9 @@ python3 /home/luke_blue/Startup_Files/import_csv_data_from_url_vaccination.py
 # 2022-02-06 full automation for app 
 python3 /home/luke_blue/Startup_Files/datawrapper_api_update_custom_lines.py
 
-python3 /home/luke_blue/Startup_Files/selenium_datawrapper.py
+#python3 /home/luke_blue/Startup_Files/selenium_datawrapper.py
+# 2022-02-11 10:27:00 UPGRADE_2.0_APP Complete
+python3 /home/luke_blue/Startup_Files/UPGRADE_2.0_APP/datawrapper_api_republish_set_for_epidemic_app.py
 
 python3 /home/luke_blue/Startup_Files/block_range_avr7d10000todaysumd_districtsxyf.py
 # 2022-01-29 - full automation for daily updates, lines: 39, 40, 55, 56, 78, 104, 125, 152
