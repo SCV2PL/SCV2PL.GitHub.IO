54b33282285bb26d4e0317726ac6e239a2daf78c
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-08-06 17:04:04 +0200

    Update startupscript.sh

--- V2/startupscript.sh
+++ V2/startupscript.sh
@@ -50,10 +50,15 @@ python3 /app/datawrapper_api_update_external_data.py
 python3 /app/datawrapper_api_republish_set_for_update_Support_-_7DAVRDLR14D_t-t14_range.py
 # 2022-02-13 09:48:00 UPGRADE_2.0_APP Complete
 # for sunday update
-#python3 /app/sunday_update/add_weekly_sheets_set.py
+python3 /app/sunday_update/add_weekly_sheets_set.py
 # 2022-02-13 09:48:00 UPGRADE_2.0_APP Complete
-# for sunday update
-#python3 /app/sunday_update/update_avr7d_array_range_7d.py
+# for sunday update & cleaning
+python3 /app/sunday_update/update_avr7d_array_range_7d.py
+# for daily cleaning
+# 2022-08-06 - full automation for update changes: loop while repeat 3 times
+python3 /app/delete_unnecessary_rows_in_SUM_sheets.py
+# 2022-08-06 - full automation for update changes
+python3 /app/update_n+4_for_config_for_delete_unnecessary_rows_in_SUM_sheets_py.py
 
 # /app/startupscript.sh
 # chmod u+r+x
