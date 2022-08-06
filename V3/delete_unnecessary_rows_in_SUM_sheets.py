265e8f3adcc0a54ca8b56d75ae06ecafcb3a42ca
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-08-06 13:53:21 +0200

    Add files via upload

--- V3/delete_unnecessary_rows_in_SUM_sheets.py
+++ V3/delete_unnecessary_rows_in_SUM_sheets.py
@@ -1,10 +1,8 @@
-# "while loop" in preparation.
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import time
 from datetime import datetime, timedelta
 import yaml
-
 
 SERVICE_ACCOUNT_FILE = '/home/blox_land/scv2pl/sars-cov-2-poland.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -19,62 +17,75 @@ spreadsheet_id2 = '1t5ndsyAp20qeOgCJrzeT_HUttgfru1SzwrhCFJzsgu0'
 service = build('sheets', 'v4', credentials=creds)
 
 sheet = service.spreadsheets()
-        
-config_vals = ""
-with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "r") as cr:
-   config_vals = yaml.full_load(cr)
-
-a = config_vals['a']
-b = config_vals['b']
-
-yesterdayn = datetime.today() - timedelta(days=b)
-c = yesterdayn.strftime('%Y%m%d')
 
 
-RUN1 = {'requests': [
-    {
-      "deleteDimension": {
-        "range": {
-          "sheetId": str(a),
-          "dimension": "ROWS",
-          "startIndex": 380,
-          "endIndex": 1000
+while True:
+
+  config_vals = ""
+  with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "r") as cr:
+     config_vals = yaml.full_load(cr)
+
+  a = config_vals['a']
+  b = config_vals['b']
+
+  yesterdayn = datetime.today() - timedelta(days=b)
+  c = yesterdayn.strftime('%Y%m%d')
+
+
+  RUN1 = {'requests': [
+      {
+        "deleteDimension": {
+          "range": {
+            "sheetId": str(a),
+            "dimension": "ROWS",
+            "startIndex": 380,
+            "endIndex": 1000
+          }
         }
-      }
-    },
+      },
 
-]}
+  ]}
 
-request1 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id1, body=RUN1).execute()
-print(request1)
+  request1 = service.spreadsheets().batchUpdate(
+          spreadsheetId=spreadsheet_id1, body=RUN1).execute()
+  print(request1)
 
 
-RUN2 = {'requests': [
-    {
-      "deleteDimension": {
-        "range": {
-          "sheetId": str(c),
-          "dimension": "ROWS",
-          "startIndex": 382,
-          "endIndex": 1000
+  RUN2 = {'requests': [
+      {
+        "deleteDimension": {
+          "range": {
+            "sheetId": str(c),
+            "dimension": "ROWS",
+            "startIndex": 382,
+            "endIndex": 1000
+          }
         }
-      }
-    },
+      },
 
-]}
+  ]}
 
-request2 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id2, body=RUN2).execute()
-print(request2)
+  request2 = service.spreadsheets().batchUpdate(
+          spreadsheetId=spreadsheet_id2, body=RUN2).execute()
+  print(request2)
 
 
-config_vals['a'] = a - 1
-with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "w") as cw:
-   yaml.dump(config_vals, cw, default_flow_style=True)
+  config_vals['a'] = a - 1
+  with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "w") as cw:
+     yaml.dump(config_vals, cw, default_flow_style=True)
    
-config_vals['b'] = b + 1
-with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "w") as cw:
-   yaml.dump(config_vals, cw, default_flow_style=True) 
+  config_vals['b'] = b + 1
+  with open("/home/blox_land/scv2pl/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "w") as cw:
+     yaml.dump(config_vals, cw, default_flow_style=True) 
    
-print("(All Operations - Successfully!)")
+  print("(All Operations - Successfully!)")
+
+  check = input("Do you want to quit or start gain, enter Y to restart or another to end ?: ")
+
+  if check.upper() == "Y": #go back to the top
+
+    continue
+  
+  print("EXIT...")
+
+  break #exit
