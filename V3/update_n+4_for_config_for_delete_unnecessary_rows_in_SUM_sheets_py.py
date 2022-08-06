116aacfecda6dd6f4b444621343e4bf68ecf3401
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-08-06 16:53:30 +0200

    Add files via upload

---
+++ V3/update_n+4_for_config_for_delete_unnecessary_rows_in_SUM_sheets_py.py
@@ -0,0 +1,13 @@
+import yaml
+
+config_vals = ""
+with open("/app/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "r") as cr:
+   config_vals = yaml.full_load(cr)
+
+n = config_vals['n']
+
+config_vals['n'] = n + 4
+with open("/app/config_for_delete_unnecessary_rows_in_SUM_sheets_py.yaml", "w") as cw:
+   yaml.dump(config_vals, cw, default_flow_style=True) 
+   
+print("(All Operations - Successfully!)")
