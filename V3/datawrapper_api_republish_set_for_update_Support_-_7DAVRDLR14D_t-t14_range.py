fce5c58471685eefba02d22be146d7b94db8b3d5
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-12-04 10:05:54 +0100

    Update datawrapper_api_republish_set_for_update_Support_-_7DAVRDLR14D_t-t14_range.py

--- V3/datawrapper_api_republish_set_for_update_Support_-_7DAVRDLR14D_t-t14_range.py
+++ V3/datawrapper_api_republish_set_for_update_Support_-_7DAVRDLR14D_t-t14_range.py
@@ -18,4 +18,55 @@ dw.publish_chart(dw_id)
 
 print(properties)
 
+
+dw_id = 'JW5zN'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
+dw_id = 'p1M7c'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
+
+dw_id = 'wt85J'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
+dw_id = 'WVcSA'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
+
+dw_id = 'uO9JC'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
+dw_id = 'C3dh5'
+
+properties = dw.chart_properties(dw_id)
+
+dw.publish_chart(dw_id)
+
+print(properties)
+
 print("All Operations - Successfully!")
