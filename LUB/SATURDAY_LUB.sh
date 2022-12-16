cc82d8f2f7d93a1ba5f367ddc370b7d7a7e94984
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-12-16 17:50:38 +0100

    Add files via upload

---
+++ LUB/SATURDAY_LUB.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+cd LUB
+
+source LUB/bin/activate
+
+python3 /home/blox_land/LUB/saturday_lub.py
+
+cd ..
+
+heroku git:clone -a scv2pl
+
+cd scv2pl
+
+git update-index --chmod=+x startupscript.sh
+
+git ls-files --stage
+
+git status
+
+git restore .
+
+git commit -am "startupscript.sh ==> 100755 mode"
+
+git push heroku main
