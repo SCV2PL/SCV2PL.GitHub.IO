5cf7e6297748326c948b94bf13de0e7b0dafbed6
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2023-01-17 14:49:39 +0100

    Update import_csv_data_from_urls.py

--- V3/import_csv_data_from_urls.py
+++ V3/import_csv_data_from_urls.py
@@ -1,9 +1,12 @@
-"""PEP 8 – Style Guide for Python Code 
+"""PEP 8 – Style Guide for Python Code
 
 https://peps.python.org/pep-0008
 
 Is this style acceptable by convention and accepted by the python community ???
 Łukasz "Luke Blue" Andruszkiewicz
+✌💙💚♻️🌌🚀🌎🌍🌏🛰🌌♻️💚💙✌
+2023-01-17 - After my #BrainFuck: my final interpretation of #Python #PEP8.
+#PyCharm #IDE
 ✌💙💚♻️🌌🚀🌎🌍🌏🛰🌌♻️💚💙✌
 """
 from google.oauth2 import service_account
@@ -11,7 +14,7 @@ from googleapiclient.discovery import build
 from datetime import datetime
 import time
 
-SERVICE_ACCOUNT_FILE = '/home/blox_land/Desktop/SCV2PL/sars-cov-2-poland.json'
+SERVICE_ACCOUNT_FILE = '/app/sars-cov-2-poland.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE,
@@ -19,14 +22,14 @@ creds = service_account.Credentials.from_service_account_file(
 service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
-voivodeships_data = 'https://api.dane.gov.pl/resources/33185,' \
-                    'aktualne-dane-dla-wojewodztw/file'
+voivodeships_data = (
+    'https://api.dane.gov.pl/resources/33185,aktualne-dane-dla-wojewodztw/file')
 
-spreadsheet_id1 = '1CGsqdT5ZF7iQjpYlNQNluOXOiS2-3ONZ0XoX-7CfLGg'
+spreadsheet_id1 = '1_iDGD9XEd5Lw_AvmVo3XkAa4rBstaemnHeLKpyTgt8g'
 
-districts_data = 'https://api.dane.gov.pl/resources/33186,' \
-                 'aktualne-dane-dla-powiatow/file'
-spreadsheet_id2 = '1xTliKwqOPDSkT9n0CiUvFC0wi0t9y9sl3kEt3S5UTvQ'
+districts_data = (
+    'https://api.dane.gov.pl/resources/33186,aktualne-dane-dla-powiatow/file')
+spreadsheet_id2 = '1JshfkqgC8bLhATHHkN3D5Bto19Sp3BpMBTYuAts5z_c'
 
 # '20220208'
 a = datetime.today().strftime('%Y%m%d')
@@ -35,17 +38,15 @@ sheet_id2 = a
 # '2022-02-08'
 b = datetime.today().strftime('%Y-%m-%d')
 
-
-RUN1 = [['=QUERY(IMPORTDATA("'+str(voivodeships_data)+'";",";";");"Select '
-                                                      'Col2,Col3,Col4,Col5,'
-                                                      'Col6,Col7,Col8,Col9,'
-                                                      'Col10,Col11,Col12,'
-                                                      'Col13,Col14,Col15,'
-                                                      'Col16,Col17,Col18,'
-                                                      'Col19,Col20")']]
+RUN1 = [
+    [('=QUERY(IMPORTDATA("'+str(voivodeships_data)+'";",";";");"Select Col2,Co'
+    'l3,Col4,Col5,Col6,Col7,Col8,Col9,Col10,Col11,Col12,Col13,Col14,Col15,Col1'
+    '6,Col17,Col18,Col19,Col20")'
+    )]
+]
 request1 = service.spreadsheets().values().update(
     spreadsheetId=spreadsheet_id1,
-    range=""+str(b)+"!A1",
+    range="" + str(b) + "!A1",
     valueInputOption="USER_ENTERED",
     body={"values": RUN1}).execute()
 time.sleep(3)
@@ -100,15 +101,15 @@ request3 = service.spreadsheets().batchUpdate(
     body=RUN3).execute()
 print(request3)
 
-
-RUN4 = [['=QUERY(IMPORTDATA("'+str(districts_data)+'",",";";"),"Select Col2,'
-                                                   'Col3,Col4,Col5,Col6,Col7,'
-                                                   'Col8,Col9,Col10,Col11,'
-                                                   'Col12,Col13,Col14,Col15,'
-                                                   'Col16,Col17,Col18,Col19,'
-                                                   'Col20,Col21")']]
+RUN4 = [
+    [('=QUERY(IMPORTDATA("'+str(districts_data)+'",",";";"),"Select Col2,Col3,'
+    'Col4,Col5,Col6,Col7,Col8,Col9,Col10,Col11,Col12,Col13,Col14,Col15,Col16,C'
+    'ol17,Col18,Col19,Col20,Col21")'
+    )]
+]
 request4 = service.spreadsheets().values().update(
-    spreadsheetId=spreadsheet_id2, range=""+str(b)+"!A1", 
+    spreadsheetId=spreadsheet_id2,
+    range="" + str(b) + "!A1",
     valueInputOption="USER_ENTERED",
     body={"values": RUN4}).execute()
 time.sleep(3)
@@ -162,6 +163,5 @@ request6 = service.spreadsheets().batchUpdate(
     spreadsheetId=spreadsheet_id2,
     body=RUN6).execute()
 print(request6)
-
 
 print("(All Operations - Successfully!)")
