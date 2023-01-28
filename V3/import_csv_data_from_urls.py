3492293a1456f138672b367490df73ac739b765e
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2023-01-28 16:15:32 +0100

    Add files via upload

--- V3/import_csv_data_from_urls.py
+++ V3/import_csv_data_from_urls.py
@@ -31,175 +31,158 @@ Source2 = config_vals['Source2']
 datetime = config_vals['datetime']
 spreadsheet_id1 = config_vals['ID1V1']
 spreadsheet_id2 = config_vals['ID2V1']
+day_n = datetime - timedelta(days=n)
+a = day_n.strftime('%Y%m%d')
+b = day_n.strftime('%Y-%m-%d')
+print(a)
+print(b)
+filepaths1 = ''+str(Source1)+''+str(a)+'.csv'
+filepaths2 = ''+str(Source2)+''+str(a)+'.csv'
+print(filepaths1)
+print(filepaths2)
 
-while n > 0:
-    config_vals = ""
-    with open("config_create_sheets.yaml", "r") \
-            as cr:
-        config_vals = yaml.full_load(cr)
-        n = config_vals['n']
+RUN1 = [['=IMPORTDATA("'+str(filepaths1)+'";",")']]
+request1 = service.spreadsheets().values().update(
+    spreadsheetId=spreadsheet_id1,
+    range=""+str(b)+"!A1",
+    valueInputOption="USER_ENTERED",
+    body={"values": RUN1}).execute()
+print(request1)
 
-        day_n = datetime - timedelta(days=n)
-        a = day_n.strftime('%Y%m%d')
-        b = day_n.strftime('%Y-%m-%d')
-        print(a)
-        print(b)
-        filepaths1 = ''+str(Source1)+''+str(a)+'.csv'
-        filepaths2 = ''+str(Source2)+''+str(a)+'.csv'
-        print(filepaths1)
-        print(filepaths2)
+RUN2 = {'requests': [
+    {'copyPaste': {
+        'source': {
+            'sheetId': a,
+            'startRowIndex': 0,
+            'endRowIndex': 18,
+            'startColumnIndex': 0,
+            'endColumnIndex': 19
+        },
+        "destination": {
+            'sheetId': a,
+            'startRowIndex': 0,
+            'endRowIndex': 18,
+            'startColumnIndex': 0,
+            'endColumnIndex': 19
+        },
+        "pasteType": "Paste_Values"
+    }}
+]}
+request2 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id1,
+    body=RUN2).execute()
+print(request2)
 
-    RUN1 = [['=IMPORTDATA("'+str(filepaths1)+'";",")']]
-    request1 = service.spreadsheets().values().update(
-        spreadsheetId=spreadsheet_id1,
-        range=""+str(b)+"!A1",
-        valueInputOption="USER_ENTERED",
-        body={"values": RUN1}).execute()
-    print(request1)
-
-    RUN2 = {'requests': [
-        {'copyPaste': {
-            'source': {
-                'sheetId': a,
-                'startRowIndex': 0,
-                'endRowIndex': 18,
-                'startColumnIndex': 0,
-                'endColumnIndex': 19
-            },
-            "destination": {
-                'sheetId': a,
-                'startRowIndex': 0,
-                'endRowIndex': 18,
-                'startColumnIndex': 0,
-                'endColumnIndex': 19
-            },
-            "pasteType": "Paste_Values"
-        }}
-    ]}
-    request2 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id1,
-        body=RUN2).execute()
-    print(request2)
-
-    RUN3 = {"requests": [
-        {"repeatCell": {
-            "range": {
-                "sheetId": a,
-                "startRowIndex": 0,
-                "endRowIndex": 18,
-                "startColumnIndex": 18,
-                "endColumnIndex": 19
-            },
-            "cell": {
-                "userEnteredFormat": {
-                    "numberFormat": {
-                        "type": "DATE_TIME",
-                        "pattern": "yyyy-mm-dd"
-                    }
-                }
-            },
-            "fields": "userEnteredFormat.numberFormat"
-        }}
-    ]}
-    request3 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id1,
-        body=RUN3).execute()
-    print(request3)
-
-    RUN31 = {'requests': [
-        {
-            "deleteDimension": {
-                "range": {
-                    "sheetId": a,
-                    "dimension": "ROWS",
-                    "startIndex": 18,
-                    "endIndex": 1000
+RUN3 = {"requests": [
+    {"repeatCell": {
+        "range": {
+            "sheetId": a,
+            "startRowIndex": 0,
+            "endRowIndex": 18,
+            "startColumnIndex": 18,
+            "endColumnIndex": 19
+        },
+        "cell": {
+            "userEnteredFormat": {
+                "numberFormat": {
+                    "type": "DATE_TIME",
+                    "pattern": "yyyy-mm-dd"
                 }
             }
-        }
-    ]}
-    request31 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id1, body=RUN31).execute()
-    print(request31)
+        },
+        "fields": "userEnteredFormat.numberFormat"
+    }}
+]}
+request3 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id1,
+    body=RUN3).execute()
+print(request3)
 
-    RUN4 = [['=IMPORTDATA("'+str(filepaths2)+'",",")']]
-    request4 = service.spreadsheets().values().update(
-        spreadsheetId=spreadsheet_id2,
-        range=""+str(b)+"!A1",
-        valueInputOption="USER_ENTERED",
-        body={"values": RUN4}).execute()
-    print(request4)
-
-    RUN5 = {'requests': [
-        {'copyPaste': {
-            'source': {
-                'sheetId': a,
-                'startRowIndex': 0,
-                'endRowIndex': 382,
-                'startColumnIndex': 0,
-                'endColumnIndex': 20
-            },
-            "destination": {
-                'sheetId': a,
-                'startRowIndex': 0,
-                'endRowIndex': 382,
-                'startColumnIndex': 0,
-                'endColumnIndex': 20
-            },
-            "pasteType": "Paste_Values"
-        }}
-    ]}
-    request5 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id2,
-        body=RUN5).execute()
-    print(request5)
-
-    RUN6 = {"requests": [
-        {"repeatCell": {
+RUN31 = {'requests': [
+    {
+        "deleteDimension": {
             "range": {
                 "sheetId": a,
-                "startRowIndex": 0,
-                "endRowIndex": 382,
-                "startColumnIndex": 19,
-                "endColumnIndex": 20
-            },
-            "cell": {
-                "userEnteredFormat": {
-                    "numberFormat": {
-                        "type": "DATE_TIME",
-                        "pattern": "yyyy-mm-dd"
-                    }
-                }
-            },
-            "fields": "userEnteredFormat.numberFormat"
-        }}
-    ]}
-    request6 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id2,
-        body=RUN6).execute()
-    print(request6)
-
-    RUN61 = {'requests': [
-        {
-            "deleteDimension": {
-                "range": {
-                    "sheetId": a,
-                    "dimension": "ROWS",
-                    "startIndex": 382,
-                    "endIndex": 1200
-                }
+                "dimension": "ROWS",
+                "startIndex": 18,
+                "endIndex": 1000
             }
         }
-    ]}
-    request61 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id2, body=RUN61).execute()
-    print(request61)
+    }
+]}
+request31 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id1, body=RUN31).execute()
+print(request31)
 
-    config_vals['n'] = n - 1
-    with open("config_create_sheets.yaml",
-              "w") as cw:
-        yaml.dump(config_vals, cw, default_flow_style=True)
+RUN4 = [['=IMPORTDATA("'+str(filepaths2)+'",",")']]
+request4 = service.spreadsheets().values().update(
+    spreadsheetId=spreadsheet_id2,
+    range=""+str(b)+"!A1",
+    valueInputOption="USER_ENTERED",
+    body={"values": RUN4}).execute()
+print(request4)
 
-    continue
+RUN5 = {'requests': [
+    {'copyPaste': {
+        'source': {
+            'sheetId': a,
+            'startRowIndex': 0,
+            'endRowIndex': 382,
+            'startColumnIndex': 0,
+            'endColumnIndex': 20
+        },
+        "destination": {
+            'sheetId': a,
+            'startRowIndex': 0,
+            'endRowIndex': 382,
+            'startColumnIndex': 0,
+            'endColumnIndex': 20
+        },
+        "pasteType": "Paste_Values"
+    }}
+]}
+request5 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id2,
+    body=RUN5).execute()
+print(request5)
 
-    break  # exit
+RUN6 = {"requests": [
+    {"repeatCell": {
+        "range": {
+            "sheetId": a,
+            "startRowIndex": 0,
+            "endRowIndex": 382,
+            "startColumnIndex": 19,
+            "endColumnIndex": 20
+        },
+        "cell": {
+            "userEnteredFormat": {
+                "numberFormat": {
+                    "type": "DATE_TIME",
+                    "pattern": "yyyy-mm-dd"
+                }
+            }
+        },
+        "fields": "userEnteredFormat.numberFormat"
+    }}
+]}
+request6 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id2,
+    body=RUN6).execute()
+print(request6)
+
+RUN61 = {'requests': [
+    {
+        "deleteDimension": {
+            "range": {
+                "sheetId": a,
+                "dimension": "ROWS",
+                "startIndex": 382,
+                "endIndex": 1200
+            }
+        }
+    }
+]}
+request61 = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheet_id2, body=RUN61).execute()
+print(request61)
